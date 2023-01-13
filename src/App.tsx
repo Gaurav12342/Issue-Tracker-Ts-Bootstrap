4481d093f87ac21234c4b5dfca8bc0ab547a0cf7
Gaurav12342 <gauravpendharkar2@gmail.com>
2023-01-13 21:58:29 +0530

    Added home component ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
-import { FC } from "react";
+import Home from "pages/Issue";
+import { FC, useEffect } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.scss";
-import Home from "./pages";
 
 const App: FC = () => {
+  useEffect(() => {
+    const unloadCallback = (event: any) => {
+      event.preventDefault();
+      event.returnValue = "";
+      return "";
+    };
+    window.addEventListener("beforeunload", unloadCallback);
+    return () => window.removeEventListener("beforeunload", unloadCallback);
+  }, [window]);
+
+  
   return (
     <div className="App">
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
---
+++ src/pages/Issue/index.tsx
@@ -0,0 +1,183 @@
+import {
+  Chip,
+  Button,
+  SearchInput,
+  Select,
+  Spinner,
+  ItemCard,
+  Pagination,
+} from "components";
+import { FC } from "react";
+import { useNavigate } from "react-router-dom";
+import "./style.scss";
+
+const Home: FC = () => {
+  const navigate: any = useNavigate();
+
+  const arrayData: any = [
+    {
+      id: "bug",
+      color: "red",
+      name: "bug",
+    },
+    {
+      id: "feature",
+      color: "blue",
+      name: "feature",
+    },
+    {
+      id: "enhancement",
+      color: "cyan",
+      name: "enhancement",
+    },
+    {
+      id: "question",
+      color: "orange",
+      name: "question",
+    },
+    {
+      id: "help",
+      color: "lime",
+      name: "help wanted",
+    },
+    {
+      id: "wontfix",
+      color: "white",
+      name: "wontfix",
+    },
+    {
+      id: "duplicate",
+      color: "rebeccapurple",
+      name: "duplicate",
+    },
+  ];
+
+  const selectArray = [
+    { name: "test", value: "test" },
+    { name: "test-1", value: "test-1" },
+    { name: "test-2", value: "test-2" },
+    { name: "test-3", value: "test-3" },
+    { name: "test-4", value: "test-4" },
+  ];
+
+  return (
+    <div className="container text-center my-5">
+      {/* Main Screen */}
+
+      <h1 className="text-white heading-text">Issue Tracker</h1>
+      <div className="row align-items-start gx-5">
+        <div className="col-7">
+          <div>
+            <SearchInput placeholder="Search" label="Search Issues" />
+          </div>
+
+          <div className=" d-flex align-items-center">
+            <p className="h4 text-white py-3">Issues List</p>
+          </div>
+
+          <div className="col-12">
+            {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0]?.map((dd: any) => {
+              return (
+                <ItemCard
+                  chipData={{ title: "bug", color: "red" }}
+                  title="Dependencies is actually working fine. I just wanted to let you
+                  know you're great all the time"
+                  description="#998 opened 1 hour ago by Tanner"
+                  numOfResponse={1000}
+                  avatarImgUrl={
+                    "https://res.cloudinary.com/uidotdev/image/twitter_name/ralex1993"
+                  }
+                />
+              );
+            })}
+          </div>
+
+          <div>
+            <Pagination nextText="Next" prevText="Previous" page={10} />
+          </div>
+        </div>
+
+        <div className="col-4 d-flex flex-column">
+          <div>
+            <label className="form-label search-label d-flex">{"Labels"}</label>
+            <div className="text-start">
+              {arrayData?.map((data: any) => {
+                return (
+                  <Chip
+                    color={data?.color}
+                    title={data?.name}
+                    onClick={() => {
+                      alert(`Number is ${data?.name}`);
+                    }}
+                  />
+                );
+              })}
+            </div>
+          </div>
+
+          <div>
+            <label className="form-label search-label d-flex mt-5">
+              {"Status"}
+            </label>
+            <div className="text-start">
+              <Select
+                onChange={(e: any) => {
+                  console.log("event", e.target.value);
+                }}
+                items={selectArray}
+              />
+            </div>
+
+            <hr className="border border-body-tertiary border-2 opacity-25"></hr>
+
+            <div className="w-100">
+              <Button>Add Issue</Button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* <ItemDetailCard /> */}
+      {/* ============================================================ */}
+
+      {/* Dummy Screen */}
+      {/* {arrayData?.map((data: any) => {
+          return (
+            <Chip
+              color={data?.color}
+              title={data?.name}
+              onClick={() => {
+                alert(`Number is ${data?.name}`);
+              }}
+            />
+          );
+        })}
+  
+        <Button>Yo yo honey sing</Button>
+  
+        <SearchInput placeholder="Search" label="Search Issues" />
+  
+        <Select
+          onChange={(e: any) => {
+            console.log("event", e.target.value);
+          }}
+          items={selectArray}
+        />
+  
+        <Spinner loading /> */}
+
+      {/* <ItemCard
+          chipData={{ title: "bug", color: "red" }}
+          title="Dependencies is actually working fine. I just wanted to let you
+                  know you're great all the time"
+          description="#998 opened 1 hour ago by Tanner"
+          numOfResponse={1000}
+          avatarImgUrl={"https://res.cloudinary.com/uidotdev/image/twitter_name/ralex1993"}
+        />
+  
+        <Pagination nextText="Next" prevText="Previous" page={10} /> */}
+    </div>
+  );
+};
+
+export default Home;
